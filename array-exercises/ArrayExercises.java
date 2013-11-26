2cbdd3e2bdf2f92e7de2eaf5bcf8d9c4aea2c2d1
B Kleinen <kleinen@infrastructure.de>
2013-11-26 11:10:40 +0100

    scaffold for exercises

---
+++ array-exercises/ArrayExercises.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class ArrayExercises here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayExercises
+{
+    public ArrayExercises()
+    {
+    }
+
+    public void printArray(int[] anArray){
+    }
+
+    public void printArray(){
+        int[] numbers = { 4, 1, 22, 9, 14, 3, 9};
+        printArray(numbers);
+    }
+
+    public int[] computeFibs(){
+        int[] fib = new int[100]; 
+        fib[0] = 0;
+        fib[1] = 1;
+        //for ...
+    }
+
+    public void printFibs(){}
+}
