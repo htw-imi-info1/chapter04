5b76d684f83e2f7b3d0323007cdc5fd907f9ad1e
B Kleinen <kleinen@infrastructure.de>
2018-05-04 10:36:34 +0200

    loop examples

---
+++ loop-examples/WhileLoop.java
@@ -0,0 +1,25 @@
+
+public class WhileLoop
+{
+    public WhileLoop()
+    {
+
+    }
+
+    public void sampleMethod()
+    {int theCurrentNumberWeArePrinting = 1;
+        while(theCurrentNumberWeArePrinting <= 10) {
+            System.out.println(theCurrentNumberWeArePrinting);
+            theCurrentNumberWeArePrinting++;
+        }
+    } 
+
+    public void multiples()
+    {int theCurrentNumberWeArePrinting = 5;
+        while(theCurrentNumberWeArePrinting <= 100) {
+            System.out.println(theCurrentNumberWeArePrinting);
+            theCurrentNumberWeArePrinting += 5;
+
+        } }
+
+}
